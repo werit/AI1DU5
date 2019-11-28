05b53a2dd17c896eaf59e124fb8bd72764146689
Werit_HP_Spectre <tib.balaz@gmail.com>
2019-11-28 10:49:06 +0100

    Initial commit

---
+++ AlphaBetaExperimentalEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FourInARow
+{
+	class AlphaBetaExperimentalEngine : AlphaBetaEngine
+	{
+		public AlphaBetaExperimentalEngine(int sizeX, int sizeY, int depth, bool isFirstPlayer) : base(sizeX, sizeY, depth, isFirstPlayer)
+		{
+		}
+
+		protected override int eval(Game g)
+		{
+			//Zde nahraďte vlastním kódem
+			return base.eval(g);
+		}
+
+		public override string getName()
+		{
+			//zde můžete napsat jméno svého enginu
+			return "Experimental " + base.getName();
+		}
+    }
+}
